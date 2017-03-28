a52667aaced31548da01f9499d17a693ace5092d
Ryan Furness <ryanfurness@northwesternmutual.com>
2017-03-28 08:50:05 -0500

    FIXED!

--- skills/trump.js
+++ skills/trump.js
@@ -2,11 +2,11 @@ var request = require('request');
 
 module.exports = function(controller){
 
-    controller.hears(['trump'],['direct_message'],function(bot,message) {
+    controller.hears(['trump'],['ambient,direct_message'],function(bot,message) {
         var start = Math.floor(Math.random() * 100);
         var url = 'https://www.googleapis.com/customsearch/v1?q=trump&num=10&start='+ start +'&cx='+ process.env.googlecx +'&searchType=image&key='+process.env.googleapi;
 
-        console.log("generated URL"+ url);
+        //console.log("generated URL"+ url);
         request({
                 method: 'get',
                 url: url
@@ -14,10 +14,10 @@ module.exports = function(controller){
             function (error, response, body) {
                 if (!error && response.statusCode === 200) {
                     var pick = Math.floor(Math.random() * 10);
-                    console.log('BODY: ', body);
+                    //console.log('BODY: ', body);
 
                     var jsonBody = JSON.parse(body);
-                    console.log('jsonbody', jsonBody);
+                    //console.log('jsonbody', jsonBody);
                     // console.log('jsonbody', jsonBody.items[0].link);
 
                     bot.reply(message, jsonBody.items[pick].link);
