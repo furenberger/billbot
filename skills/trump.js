4b6da476a3ce864f333bed030d3a9253fd6cc4e0
Ryan Furness <ryanfurness@northwesternmutual.com>
2017-03-28 08:33:50 -0500

    error log

--- skills/trump.js
+++ skills/trump.js
@@ -2,7 +2,7 @@ var request = require('request');
 
 module.exports = function(controller){
 
-    controller.hears(['trump'],['ambient,direct_message,direct_mention,mention'],function(bot,message) {
+    controller.hears(['trump'],['direct_message'],function(bot,message) {
         var start = Math.floor(Math.random() * 100);
         var url = 'https://www.googleapis.com/customsearch/v1?q=trump&num=10&start='+ start +'&cx='+ process.env.googlecx +'&searchType=image&key='+process.env.googleapi;
 
@@ -12,6 +12,10 @@ module.exports = function(controller){
                 url: url
             },
             function (error, response, body) {
+                if(error)
+                {
+                    bot.reply(error);
+                }
                 if (!error && response.statusCode === 200) {
                     var pick = Math.floor(Math.random() * 10);
                     // console.log('BODY: ', body);
