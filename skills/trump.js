cdddc936ff38e1a2d07751ee501753ffc55f360a
Ryan Furness <ryanfurness@northwesternmutual.com>
2017-03-28 09:42:46 -0500

    logs on

--- skills/trump.js
+++ skills/trump.js
@@ -6,7 +6,7 @@ module.exports = function(controller){
         var start = Math.floor(Math.random() * 100);
         var url = 'https://www.googleapis.com/customsearch/v1?q=trump&num=10&start='+ start +'&cx='+ process.env.googlecx +'&searchType=image&key='+process.env.googleapi;
 
-        //console.log("generated URL"+ url);
+        console.log("generated URL"+ url);
         request({
                 method: 'get',
                 url: url
@@ -14,11 +14,11 @@ module.exports = function(controller){
             function (error, response, body) {
                 if (!error && response.statusCode === 200) {
                     var pick = Math.floor(Math.random() * 10);
-                    //console.log('BODY: ', body);
+                    console.log('BODY: ', body);
 
                     var jsonBody = JSON.parse(body);
-                    //console.log('jsonbody', jsonBody);
-                    // console.log('jsonbody', jsonBody.items[0].link);
+                    console.log('jsonbody', jsonBody);
+                    console.log('jsonbody', jsonBody.items[0].link);
 
                     bot.reply(message, jsonBody.items[pick].link);
                 }
