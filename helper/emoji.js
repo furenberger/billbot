723d530dd43c6e9edcd795224c6c7523a4154ed9
Ryan Furness <ryanfurness@northwesternmutual.com>
2017-03-29 16:17:00 -0500

    insulting bill

--- helper/emoji.js
+++ helper/emoji.js
@@ -1,11 +1,54 @@
 /*
     attach emoji to message
 */
-module.exports = function(bot, message) {
+module.exports = function(bot, message, specific) {
+    var emoji = [
+        'bill_emoji',
+        'bill_emoji',
+        'bill_emoji',
+        'bill_emoji',
+        'bill_emoji',
+        'aliensguy',
+        'awwyeah',
+        'aw_yeah',
+        'balloonface',
+        'blondesassyparrot',
+        'bob_ross',
+        'briefcase_wanker',
+        'challenge_accepted',
+        'coolio',
+        'crying_jordan',
+        'crying_kim_kardashian',
+        'deadpool',
+        'dickbutt',
+        'doom_flame_barrel',
+        'doom_mad',
+        'ermygerd',
+        'facepalm',
+        'evil',
+        'excellent',
+        'nicmoji_laugh',
+        'mooning',
+        'partyparrot',
+        'hypnotoad',
+        'hypnotoad2',
+        'i_see_what_you_did_there',
+        'imposibru',
+        'you_dont_say'
+    ];
+    var index = Math.floor(Math.random() * emoji.length);
+    var emoji = emoji[index];
+
+    if(specific){
+        emoji = specific;
+    }
+
+    console.log("emoji: " + emoji);
+
     bot.api.reactions.add({
         timestamp: message.ts,
         channel: message.channel,
-        name: 'bill_emoji'
+        name: emoji
     }, function (err, res) {
         if (err) {
             bot.botkit.log('Failed to add emoji reaction :(', err);
---
+++ helper/insult.js
@@ -0,0 +1,17 @@
+var request = require('request');
+
+module.exports = function(bot, message){
+    //http://autoinsult.datahamster.com/index.php?style=3
+    //<?xml version="1.0" encoding="utf-8" ?><xjx><cmd n="as" t="insult" p="innerHTML"><![CDATA[You gruesome box of hideous dog barf]]></cmd></xjx>
+    request({
+            method: 'get',
+            url: 'http://autoinsult.datahamster.com/scripts/webinsult.server.php?xajax=generate_insult&xajaxargs[]=3&xajaxr=1490819484403'
+        },
+        function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                // console.log('BODY: ', body);
+                var insult = body.substring(body.indexOf('[CDATA[')+7,body.indexOf(']]'));
+                bot.reply(message, insult);
+            }
+        }).end('{}');
+};
