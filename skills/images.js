cff6bf08299efad8caef2b079d76909e40c1a352
Ryan Furness <ryanfurness@northwesternmutual.com>
2017-04-06 20:23:16 -0500

    slackbot -> bot

--- skills/images.js
+++ skills/images.js
@@ -11,7 +11,7 @@ module.exports = function(controller){
             .then(function (image) {
                 bot.reply(message, image);
             }).catch(function (err) {
-            slackBot.api.reactions.add({
+            bot.api.reactions.add({
                 channel: '#general',
                 name: 'mic_drop'
             }, function (err, res) {
@@ -28,7 +28,7 @@ module.exports = function(controller){
             .then(function (image) {
                 bot.reply(message, image);
             }).catch(function (err) {
-            slackBot.api.reactions.add({
+            bot.api.reactions.add({
                 channel: '#general',
                 name: 'finger_hole'
             }, function (err, res) {
