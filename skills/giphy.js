67097e265efed1b5f1882d719c3cfea108d8a3ba
Ryan Furness <ryanfurness@northwesternmutual.com>
2017-05-03 14:48:23 -0500

    pg-13

--- skills/giphy.js
+++ skills/giphy.js
@@ -5,7 +5,7 @@ const randomnumber = require('../addons/randomnumber');
 
 module.exports = () => {
     return new Promise((resolve, reject) => {
-        request("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=random&fmt=json", (error, response, body) => {
+        request("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=random&fmt=json&rating=pg-13", (error, response, body) => {
             const data = JSON.parse(body);
 
             const gifUrl = data.data.fixed_height_downsampled_url;
